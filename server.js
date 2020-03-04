a9c58a44bbbbb81bc8e1b87e4b37606554a58738
HappyPantss <sergioeijben@hotmail.com>
2020-03-04 15:43:57 +0100

    Tried to view data from server

--- server.js
+++ server.js
@@ -13,9 +13,33 @@ app.set('views', 'view')
 // app.get('/', (req, res) =>
 // 	res.sendfile(path.join(__dirname + 'static/register.html')))
 
-app.get('/', (req, res) =>
-	res.render('login.ejs'))
+// function movies(req, res) {
+// 	var doc = '<!doctype html>'
+// 	var length = data.length
+// 	var index = -1
+// 	var movie
+	
+// 	doc += '<title>My movie website</title>'
+// 	doc += '<h1>Movies</h1>'
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+// 	while (++index < length) {
+// 		movie = data[index]
+// 		doc += '<h2><a href="/' + movie.id + '">' + movie.title + '</a></h2>'
+// 		doc += '<p>' + movie.plot + '</p>'
+// 	}
+// }
+
+var data = [
+	{
+	  title: 'Lord of the Rings',
+	  plot: 'The Fellowship of the Ring embark on a journey to destroy the One Ring and end Sauron`s reign over Middle-earth.',
+	  description: 'Five friends head to a remote …'
+	}
+  ];
+
+app.get('/', (req, res, next) =>
+	res.render('login.ejs', {data: title}))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // app.get('/', (req, res) => res.send('Hello World!'))
