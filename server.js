64a6a2118806d09d13531c113a3a8825b90feb97
HappyPantss <sergioeijben@hotmail.com>
2020-03-10 10:05:08 +0100

    Update

--- server.js
+++ server.js
@@ -1,82 +1,57 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const body = require('body-parser');
 const slug = require('slug');
-const bodyParser = require('body-parser');
 const path = require('path');
 const multer = require('multer');
 
 express()
 	.use(express.static('static'))
-	.use(bodyParser.urlencoded({extended: true}))
+	.use(express.urlencoded({extended: true}))
 	.use('/static', express.static('static'))
 	.set('view engine', 'ejs')
-	.get('/', movies)
-	.get('/add', form)
-	.get('/:id', movie)
-	.post('/', add)
+	.listen(8000)
 
-// app.use('/static', express.static('static'));
-// app.get('/', (req, res) =>
-// 	res.sendfile(path.join(__dirname + 'static/index.html')))
+let data = [{
+	  	title: 'Lord of the Rings',
+	  	plot: 'The Fellowship of the Ring embark on a journey to destroy the One Ring and end Sauron`s reign over Middle-earth.',
+	  	description: 'The Lord of the Rings is the saga of a group of sometimes reluctant heroes..'
+	}];
 
-// app.get('/', (req, res) =>
-// 	res.sendfile(path.join(__dirname + 'static/register.html')))
-
-function movies(req, res) {
+app.get('/', (req, res, next) => {
 	res.render('login.ejs', {data: data})
-}
+});
 
-function movie(req, res, next) {
-	let id = req.params.id
-	var movie = find(data, function (value){
-		return value.id === id
-	})
+app.get('/detail', (req, res, next) => {
+	res.render('detail.ejs', {data: data})
+});
 
-	if(!movie) {
-		next()
-		return
-	}
-
-	res.render('detail.ejs', {data: movie})
-}
-
-function form(req, res) {
-	res.render('add.ejs')
-}
-
-function add(req, res) {
-	let id = slug(req.body.title).toLowerCase()
-
-	console.log(add)
+app.post('/addmovie', (req, res, next) =>
+{
+	console.log(req.body)
+  	const title = req.body.title;
+  	const plot = req.body.plot;
+  	const description = req.body.description;
 	
 	data.push({
-		id: id,
+		title: title,
+	  	plot: plot,
+	  	description: description
+  })
+
+  res.redirect('/')
+});
+
+function add(req, res) {
+	data.push({
 		title: req.body.title,
 		plot: req.body.plot,
 		description: req.body.description
 	})
 
-	res.redirect('/login' + id)
+	res.redirect('/detail')
 }
-
-let data = [
-	{
-		id: 'lord-of-the-rings',
-	  	title: 'Lord of the Rings',
-	  	plot: 'The Fellowship of the Ring embark on a journey to destroy the One Ring and end Sauron`s reign over Middle-earth.',
-	  	description: 'The Lord of the Rings is the saga of a group of sometimes reluctant heroes..'
-	}
-  ]
-
-app.get('/', (req, res, next) =>
-	res.render('login.ejs', {data: data}))
-
-app.get('/', (req, res, next) =>
-	res.render('detail.ejs', {data: data}))
-
-app.get('/', (req, res, next) =>
-	res.render('add.ejs', {data: data}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
