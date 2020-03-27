72351680e0d0327d6e2a6ee44ccbda62139d5dda
Stan <Stanbankras@gmail.com>
2020-03-27 17:14:23 +0100

    Cleaned up code, added comments where I think you can improve

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const slug = require('slug');
-const path = require('path');
-const multer = require('multer');
 const urlencodedParser = bodyParser.urlencoded({
 	extended: true
 });
@@ -18,25 +15,52 @@ let db = null;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(() => {
-  db = client.db(process.env.DB_NAME);
-});
-
-mongo.MongoClient.connect(uri, function (err, client) {
+client.connect((err) => {
 	if (err) {
 		throw err
 	}
-
-	db = client.db(process.env.DB_NAME)
-	// console.log(db)
-})
+  db = client.db(process.env.DB_NAME);
+});
 
 app.use(express.static('static'))
 app.use(express.urlencoded({extended: true}))
 app.use('/static', express.static('static'))
 app.set('view engine', 'ejs') // Makes sure we use EJS as a templating engine
 
-// read data from DB
+// Render EJS to HTML
+app.get('/allUsers', users)
+
+app.get('/detail/:id/' + users._id + '', users)
+
+app.get('/detail', users)
+
+app.post('/', add)
+
+app.get('/', (req, res, next) => {
+	res.render('login.ejs')
+});
+
+app.get('/register', (req, res, next) => {
+	res.render('register.ejs')
+});
+
+app.get('/detail', (req, res, next) => {
+	res.render('detail.ejs')
+});
+
+app.get('/detail/:id/', async (req, res, next) => {
+	try {
+		const profile = await db.collection('user').findOne({ _id: mongo.ObjectID(req.params.id) })	
+		res.render('detail.ejs', {user: profile})
+	} catch(err) {
+		console.log(err);
+	}
+});
+
+app.get('/forgotPass', (req, res, next) => {
+	res.render('forgotPass.ejs')
+});
+
 function users(req, res, next) {
 	db.collection('user').find().toArray(done)
 
@@ -101,54 +125,4 @@ function login(req) {
 // 	}
 // }
 
-// Render EJS to HTML
-app.get('/allUsers', users)
-
-app.get('/detail/:id/' + users._id + '', users)
-
-// app.get('/detail/' + users.username + '', users)
-
-app.get('/detail', users)
-
-app.post('/', add)
-
-// app.post('/login', login)
-
-// app.post('/', remove)
-
-app.get('/', (req, res, next) => {
-	res.render('login.ejs')
-});
-
-app.get('/register', (req, res, next) => {
-	res.render('register.ejs')
-});
-
-// app.get('/allUsers', (req, res, next) => {
-// 	res.render('allUsers.ejs')
-// });
-
-app.get('/detail', (req, res, next) => {
-	res.render('detail.ejs')
-});
-
-// app.get('/detail?id=' + users._id + '', (req, res, next) => {
-// 	res.render('detail.ejs')
-// });
-
-app.get('/detail/:id/', async (req, res, next) => {
-	try {
-		const profile = await db.collection('user').findOne({ _id: mongo.ObjectID(req.params.id) })	
-		res.render('detail.ejs', {user: profile})
-	} catch(err) {
-		console.log(err);
-	}
-});
-
-app.get('/forgotPass', (req, res, next) => {
-	res.render('forgotPass.ejs')
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-// app.get('/', (req, res) => res.send('Hello World!'))
